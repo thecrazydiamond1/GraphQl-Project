4ac70861a237d5dc947297d6a6c6895a15de46c1
Jeevan Joshi <joshijeewon18@gmail.com>
2025-10-21 21:02:03 +0545

    feat: set up backend for graphql successfully using apollo server

---
+++ server/server.js
@@ -0,0 +1,57 @@
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+
+const users=[
+    {id:"1", name:"Alice Smith", age:30, isMarried:false},
+    {id:"2", name:"Bob Doe", age:25, isMarried:true},
+    {id:"3", name:"Charlie Johnson", age:35, isMarried:false},
+]
+const typeDefs=`
+    type Query{
+        getUsers: [User]
+        getUserById(id: ID!): User
+
+    }
+    type Mutation{
+        createUser(name: String!, age: Int!, isMarried: Boolean!): User
+    }
+    type User{
+        id: ID
+        name: String
+        age: Int
+        isMarried: Boolean
+    }
+`;
+const resolvers={
+    Query:{
+        getUsers: ()=>{
+            return users;
+        },
+        getUserById: (parent, args)=>{
+            const id = args.id
+            return users.find(user => user.id === id);
+        }
+
+    },
+    Mutation:{
+        createUser: (parent, args)=>{
+            const {name, age, isMarried}=args;
+            const newUser ={
+                id: String(users.length + 1),
+                name,
+                age,
+                isMarried
+            };
+            users.push(newUser);
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+const{url}= await startStandaloneServer(server, {
+    listen:{port:4000}
+});
+console.log(`Server running at ${url}`);
+
+/// Query, Mutuation, typeDefs, resolvers
